b8e740e02db5f81df21bbe328031280c600a021a
Brecht Waeghe <brecht@imac-van-brecht.lan>
2014-08-20 08:58:50 +0200

    ck unleash

---
+++ assets/js/ck_unleash.js
@@ -0,0 +1,52 @@
+if(typeof(CKEDITOR) !== 'undefined') {
+	CKEDITOR.stylesSet.add( 'my_styles', [
+	    {
+                name : 'Image left',
+                element : 'img',
+                attributes :
+                {
+                        'class' : 'left'
+                }
+        },
+        {
+                name : 'Image right',
+                element : 'img',
+                attributes :
+                {
+                        'class' : 'right'
+                }
+        },
+        {
+                name : 'Align left',
+                element : ['div', 'p'],
+                attributes :
+                {
+                        'class' : 'align-left'
+                }
+        },
+        {
+                name : 'Align right',
+                element : ['div', 'p'],
+                attributes :
+                {
+                        'class' : 'align-right'
+                }
+        },
+        {
+                name : 'Align center',
+                element : ['div', 'p'],
+                attributes :
+                {
+                        'class' : 'align-center'
+                }
+        },
+        {
+                name : 'Clearfix',
+                element : ['div', 'p', 'ul'],
+                attributes :
+                {
+                        'class' : 'clearfix'
+                }
+        }
+	]);
+}

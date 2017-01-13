058fa0b969e0f8e69e76555081db83c4ef66d705
Brecht <brecht@esign.eu>
2017-01-13 09:35:48 +0100

    Gulp update: removed blade2nunjucks, improved nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,18 @@
 // Load plugins
 var gulp = require('gulp'),
     elixir = require('laravel-elixir'),
-    blade2nunjucks = require('gulp-blade2nunjucks'),
+    nunjucksRender = require('gulp-nunjucks-render'),
     nunjucks = require('gulp-nunjucks'),
     notify = require('gulp-notify'),
     del = require('del');
 
 elixir.config.css.autoprefix.options.browsers = ['> 1%', 'Last 2 versions', 'IE 9', 'IE 8'];
 
-// Styles
-gulp.task('styles', function() {
-
-});
-
-// Scripts
-gulp.task('scripts', function() {
-
-});
-
-// Images
-gulp.task('images', function() {
-
-});
-
 // Templates
 gulp.task('templates', function() {
-  gulp.src(['resources/templates/**/*.blade.html'])
-    .pipe(blade2nunjucks({
-      ext: '.html'
-    }))
-    .pipe(gulp.dest('./resources/nunjucks/'));
-
-  gulp.src(['resources/nunjucks/*.html'])
-    .pipe(nunjucks.compile({
-      name: function(file) {
-        return file.relative.replace(/\\/, '\/')//Windows path.sep to Linux path.sep
-      }
+  gulp.src(['resources/nunjucks/**/*.+(html|nunjucks)'])
+    .pipe(nunjucksRender({
+      path: ['resources/nunjucks']
     }))
     .pipe(gulp.dest('./public/'))
     .pipe(notify({ message: 'templates complete' }));
@@ -43,7 +20,7 @@ gulp.task('templates', function() {
 
 // Clean
 gulp.task('clean', function() {
-  return del([ 'public/*.html', 'resources/nunjucks', 'public/assets/css', 'public/assets/js', 'public/assets/images']);
+  return del([ 'public/*.html', 'public/assets/css', 'public/assets/js', 'public/assets/images']);
 });
 
 // Default task
@@ -51,33 +28,22 @@ gulp.task('default', ['clean'], function() {
   gulp.start('styles', 'scripts', 'images', 'templates');
 });
 
-// Watch
-
-  //gulp.task('watch', function() {
-
-  // Watch templates
-    //gulp.watch('resources/templates/**/*.blade.html', ['templates']);
-
-  // Watch .less files
-  //gulp.watch('src/assets/css/**/*.less', ['styles']);
-
-  // Watch .js files
-  //gulp.watch('src/assets/js/**/*.js', ['scripts']);
-
-  // Watch image files
-  //gulp.watch('src/assets/img/**/*', ['images']);
-
-  //});
-
 elixir(function(mix) {
+
+  // Templates
+  gulp.watch('resources/nunjucks/**/*.+(html|nunjucks)', ['templates']);
+
+  // Styles
   mix.less('style.less', 'public/assets/css/style.css');
 
+  // Scripts
   mix.scripts(['libs/modernizr.min.js'], 'public/js/head.js')
      .scripts(['libs/jquery.min.js', 'plugins.js', 'esign.js'], 'public/js/app.js');
 
+  // Browsersync
   mix.browserSync({
     // If you are developing locally point this to your local url
-    proxy: 'http://localhost:8888/public',
+    proxy: 'http://localhost:8888/_template/public',
     notify: false
   });
 });

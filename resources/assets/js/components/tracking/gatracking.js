bcc37e3c8dfccbd8b5bb9c472c78be50cbb9ae7d
bartdeco <bart@esign.eu>
2019-04-01 14:19:56 +0200

    fix eslint issues ga_tracking

--- resources/assets/js/components/tracking/gatracking.js
+++ resources/assets/js/components/tracking/gatracking.js
@@ -1,63 +1,53 @@
 import $ from 'jquery';
 
 export default function () {
+  const external = [];
+  const mails = [];
+  const files = [];
+  const trackbtns = [];
+  const filetypes = ['pdf', 'jpg'];
+  const links = $('a');
+  const lLength = links.length;
+  for (let i = 0; i < lLength; i += 1) {
+    const $elem = $(links[i]);
+    const href = $elem.attr('href');
 
-    var external = [];
-    var mails = [];
-    var files = [];
-    var trackbtns = [];
-    var filetypes = ['pdf','jpg'];
-    var links = $('a');
-    var l_length = links.length;
-    for (i=0;i<l_length;i++)
-    {
-        var $elem = $(links[i]);
-        var href = $elem.attr('href');
-
-        if (href !== undefined)
-        {
-            if ((href.match(/^https?\:/i)) && (!href.match(document.domain)))
-            {
-                external.push(links[i]);
-            }
-            else if (href.match(/^mailto\:/i))
-            {
-                mails.push(links[i]);
-            }
-            else if ($.inArray(href.split('.')[href.split('.').length - 1], filetypes) >= 0)
-            {
-                files.push(links[i]);
-            }
-            else if($elem.hasClass('tracking'))
-            {
-                trackbtns.push(links[i]);
-            }
-        }
+    if (href !== undefined) {
+      if ((href.match(/^https?:/i)) && (!href.match(document.domain))) {
+        external.push(links[i]);
+      } else if (href.match(/^mailto:/i)) {
+        mails.push(links[i]);
+      } else if ($.inArray(href.split('.')[href.split('.').length - 1], filetypes) >= 0) {
+        files.push(links[i]);
+      } else if ($elem.hasClass('tracking')) {
+        trackbtns.push(links[i]);
+      }
     }
+  }
 
-    $(external).on('click', function(){
-        var link = $(this);
-        var linktxt = (link.attr('title') !== '') ? link.attr('title') + ' [' + link.attr('href').replace(/^https?\:\/\//i, '') + ']' : link.attr('href').replace(/^https?\:\/\//i, '');
-        ga('send', 'event', 'external', 'visit', linktxt);
-    });
+  $(external).on('click', () => {
+    const link = $(this);
+    const linkTxt = link.attr('title') !== '' ? `${link.attr('title')} [${link.attr('href').replace(/^https?:\/\//i, '')} ]` : link.attr('href').replace(/^https?:\/\//i, '');
+    ga('send', 'event', 'external', 'visit', linkTxt); // eslint-disable-line no-undef
+  });
 
-    $(mails).on('click', function(){
-        var email = $(this);
-        var emailtxt = email.attr('href').substring(7);
-        ga('send', 'event', 'mailto', 'click', emailtxt);
-    });
+  $(mails).on('click', () => {
+    const email = $(this);
+    const emailTxt = email.attr('href').substring(7);
+    ga('send', 'event', 'mailto', 'click', emailTxt); // eslint-disable-line no-undef
+  });
 
-    $(files).on('click', function(){
-        var file = $(this);
-        var filehref = file.attr('href');
-        var fileext = filehref.split('.')[filehref.split('.').length - 1];
-        var filename = filehref.substring(filehref.lastIndexOf('/')+1);
-        ga('send', 'event', 'download', fileext, filename);
-    });
+  $(files).on('click', () => {
+    const file = $(this);
+    const fileHref = file.attr('href');
+    const fileExt = fileHref.split('.')[fileHref.split('.').length - 1];
+    const filename = fileHref.substring(fileHref.lastIndexOf('/') + 1);
+    ga('send', 'event', 'download', fileExt, filename); // eslint-disable-line no-undef
+  });
 
-    $(trackbtns).on('click', function(){
-        var link = $(this);
-        var txt = (link.data('title') !== 'undefined') ? link.data('title') : link.attr('href').replace(/^https?\:\/\//i, '');
-        ga('send', 'event', 'button', 'click', txt);
-    });
+  $(trackbtns).on('click', () => {
+    const link = $(this);
+    const txt = (link.data('title') !== 'undefined') ? link.data('title') : link.attr('href').replace(/^https?:\/\//i, '');
+    ga('send', 'event', 'button', 'click', txt); // eslint-disable-line no-undef
+  });
 }

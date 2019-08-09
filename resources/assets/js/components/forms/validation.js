8ff7062bdcd3b01be591079d246a470b1c5f13b0
arne <arne@esign.eu>
2019-08-09 16:56:58 +0200

    inline form validation WIP

--- resources/assets/js/components/forms/validation.js
+++ resources/assets/js/components/forms/validation.js
@@ -1,13 +1,20 @@
-import $ from 'jquery';
-import { isMobile } from '../../utils/isMobile';
-
-require('../validationengine/languages/jquery.validationEngine-nl');
-require('../validationengine/jquery.validationEngine');
+import { Validation } from 'bunnyjs/src/Validation';
+import { validationEn } from './validationTranslations';
 
 export default function () {
-  if (isMobile) {
-    $('.validate').validationEngine();
-  } else {
-    $('.validate').validationEngine({ scroll: false });
-  }
+  Validation.ui.config = {
+    classInputGroup: 'input-group',
+    classInputGroupError: 'input-group--has-error',
+    classLabel: 'input-group__label',
+    tagNameError: 'small',
+    classError: 'input-group__error',
+    selectorInput: '[name]',
+  };
+
+  // TODO: load correct language from html lang attribute
+  Validation.lang = validationEn;
+
+  [...document.querySelectorAll('form.validate')].forEach((form) => {
+    Validation.init(form, true);
+  });
 }
---
+++ resources/assets/js/components/forms/validationTranslations.js
@@ -0,0 +1,18 @@
+export const validationEn = {
+  required: '\'{label}\' is required',
+  email: '\'{label}\' should be a valid e-mail address',
+  tel: '\'{label}\' is not a valid telephone number',
+  maxLength: '\'{label}\' length must be < {maxLength}',
+  minLength: '\'{label}\' length must be > {minLength}',
+  maxFileSize: 'Max file size must be < {maxFileSize}MB, uploaded {fileSize}MB',
+  image: '\'{label}\' should be an image (JPG or PNG)',
+  minImageDimensions: '\'{label}\' must be > {minWidth}x{minHeight}, uploaded {width}x{height}',
+  maxImageDimensions: '\'{label}\' must be < {maxWidth}x{maxHeight}, uploaded {width}x{height}',
+  requiredFromList: 'Select \'{label}\' from list',
+  confirmation: '\'{label}\' is not equal to {originalLabel}',
+  minOptions: 'Please select at least {minOptionsCount} options',
+};
+
+export const validationNl = {
+
+};

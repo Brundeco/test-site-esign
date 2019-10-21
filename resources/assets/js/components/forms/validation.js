48ea5d8b67a03cc627b563312b352e2f23439b4f
Brecht <brecht@esign.eu>
2019-10-21 15:05:19 +0200

    Hyperform validation

--- resources/assets/js/components/forms/validation.js
+++ resources/assets/js/components/forms/validation.js
@@ -1,37 +1,39 @@
-import { Validation } from 'bunnyjs/src/Validation';
-import {
-  validationEn,
-  validationNl,
-  validationFr,
-  validationDe,
-} from './validationTranslations';
+import hyperform from 'hyperform';
 
-const getLang = () => {
-  switch (document.documentElement.lang) {
-    case 'nl':
-      return validationNl;
-    case 'fr':
-      return validationFr;
-    case 'de':
-      return validationDe;
-    default:
-      return validationEn;
-  }
-};
+import { setHyperformLang } from './validationTranslations';
 
 export default function () {
-  Validation.ui.config = {
-    classInputGroup: 'input-group',
-    classInputGroupError: 'input-group--has-error',
-    classLabel: 'input-group__label',
-    tagNameError: 'small',
-    classError: 'input-group__error',
-    selectorInput: '[name]',
-  };
-
-  Validation.lang = getLang();
+  setHyperformLang();
 
   [...document.querySelectorAll('form.validate')].forEach((form) => {
-    Validation.init(form, true);
+    hyperform(form, {
+      revalidate: 'onblur',
+      classes: {
+        warning: 'input-group__error',
+      },
+    });
+  });
+
+  const inputClass = 'input-group';
+  const errorClass = 'input-group--error';
+
+  // changed warning renderer
+  hyperform.setRenderer('attachWarning', (warning, element) => {
+    const parent = element.parentNode;
+    parent.appendChild(warning);
+
+    if (parent.classList.contains(inputClass)) {
+      parent.classList.add(errorClass);
+    } else if (parent.parentNode.classList.contains(inputClass)) {
+      parent.parentNode.classList.add(errorClass);
+    }
+  });
+
+  // set detach
+  hyperform.setRenderer('detachWarning', (warning, element) => {
+    const parent = element.parentNode;
+    parent.removeChild(warning);
+    parent.classList.remove(errorClass);
+    parent.parentNode.classList.remove(errorClass);
   });
 }

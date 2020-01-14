fb58cebe19368584c30455d04f21c132c4d89f42
cilia <cilia@esign.eu>
2020-01-14 14:05:10 +0100

    fixed navigation & collapse

--- resources/assets/js/components/layout/navigation.js
+++ resources/assets/js/components/layout/navigation.js
@@ -1,4 +1,16 @@
 
 export default function () {
-
+  const $header = $('.page-header--fixed');
+  let lastScrollTop = 0;
+  $(window).scroll(function () {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st > lastScrollTop) {
+      $header.addClass('page-header--collapsed');
+    } else {
+    }
+    if (st < 50) {
+      $header.removeClass('page-header--collapsed');
+    }
+    lastScrollTop = st;
+  });
 }

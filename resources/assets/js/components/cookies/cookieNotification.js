eedfe667fd852ca360e2beacf163e8f4633d150c
Thomas Dhondt <thomas@esign.eu>
2020-05-14 10:48:52 +0200

    Replace cookiejs with js-cookie

--- resources/assets/js/components/cookies/cookieNotification.js
+++ resources/assets/js/components/cookies/cookieNotification.js
@@ -1,12 +1,12 @@
-import cookie from 'cookiejs';
+import Cookies from 'js-cookie';
 
 export default function() {
-  if (!cookie.get('cookie-consent')) {
+  if (!Cookies.get('cookie-consent')) {
     const cookieNotificationEl = document.getElementById('cookie-notification');
     const acceptCookiesTrigger = document.getElementById('accept-cookies');
 
     acceptCookiesTrigger.addEventListener('click', () => {
-      cookie.set('cookie-consent', '1', 365);
+      Cookies.set('cookie-consent', '1', { expires: 365 });
       cookieNotificationEl.classList.add('hide');
     });
 

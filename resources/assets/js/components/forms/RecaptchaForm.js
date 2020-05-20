374ca1713728e0073af942874a61d5092b28b130
bartdeco <bart@esign.eu>
2020-05-20 12:28:46 +0200

    Fix: multiple events should be whitespace separated while using jQuery.one()

--- resources/assets/js/components/forms/RecaptchaForm.js
+++ resources/assets/js/components/forms/RecaptchaForm.js
@@ -26,7 +26,7 @@ export default class RecaptchaForm {
 
     // Only initialize recaptcha when it is actually needed instead of on pageload
     // Recaptcha script tag should have a [data-src] containing the value of the regular [src]
-    this.$form.one('focusin,input,change', () => this.lFocus());
+    this.$form.one('focusin input change', () => this.lFocus());
   }
 
   lFocus() {

ac8e59e8b65062dbfdc49fa6cb4b693ab6e4d71a
bartdeco <bart@esign.eu>
2020-06-17 21:27:26 +0200

    Update: "require" the icons, but comment those lines due to build failing when the files do not yet exist

--- resources/views/layouts/_partials/head.blade.php
+++ resources/views/layouts/_partials/head.blade.php
@@ -13,7 +13,7 @@
   <meta property="og:title" content="{{ $siteTitle }}">
   <meta property="og:description" content="{{ $description }}" >
   <meta property="og:url" content="nl">
-  <meta property="og:image" content="../assets/images/share-image.jpg" >
+  {{-- TODO <meta property="og:image" content="${require(`../assets/images/share-image.jpg`)}"> --}}
   <meta property="og:site_name" content="Template">
   <meta property="og:type" content="website">
   {{-- <meta property="article:author" content=""> --}}
@@ -22,28 +22,28 @@
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="{{ $siteTitle }}">
   <meta name="twitter:description" content="{{ $description }}">
-  <meta name="twitter:image" content="../../assets/images/share-image.jpg">
+  {{-- TODO <meta name="twitter:image" content="${require(`../../assets/images/share-image.jpg`)}"> --}}
 
-  <link rel="shortcut icon" href="../../assets/images/icons/favicon.ico" type="image/x-icon">
-  <link rel="apple-touch-icon" sizes="57x57" href="../../assets/images/icons/apple-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="../../assets/images/icons/apple-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="../../assets/images/icons/apple-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="../../assets/images/icons/apple-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="../../assets/images/icons/apple-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="../../assets/images/icons/apple-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="../../assets/images/icons/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="../../assets/images/icons/apple-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="../../assets/images/icons/apple-icon-180x180.png">
-  <link rel="icon" type="image/png" sizes="192x192"  href="../../assets/images/icons/android-icon-192x192.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="../../assets/images/icons/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="../../assets/images/icons/favicon-96x96.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/icons/favicon-16x16.png">
+  {{-- TODO <link rel="shortcut icon" href="${require(`../../assets/images/icons/favicon.ico`)}" type="image/x-icon"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="57x57" href="${require(`../../assets/images/icons/apple-icon-57x57.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="60x60" href="${require(`../../assets/images/icons/apple-icon-60x60.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="72x72" href="${require(`../../assets/images/icons/apple-icon-72x72.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="76x76" href="${require(`../../assets/images/icons/apple-icon-76x76.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="114x114" href="${require(`../../assets/images/icons/apple-icon-114x114.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="120x120" href="${require(`../../assets/images/icons/apple-icon-120x120.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="144x144" href="${require(`../../assets/images/icons/apple-icon-144x144.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="152x152" href="${require(`../../assets/images/icons/apple-icon-152x152.png`)}"> --}}
+  {{-- TODO <link rel="apple-touch-icon" sizes="180x180" href="${require(`../../assets/images/icons/apple-icon-180x180.png`)}"> --}}
+  {{-- TODO <link rel="icon" type="image/png" sizes="192x192"  href="${require(`../../assets/images/icons/android-icon-192x192.png`)}"> --}}
+  {{-- TODO <link rel="icon" type="image/png" sizes="32x32" href="${require(`../../assets/images/icons/favicon-32x32.png`)}"> --}}
+  {{-- TODO <link rel="icon" type="image/png" sizes="96x96" href="${require(`../../assets/images/icons/favicon-96x96.png`)}"> --}}
+  {{-- TODO <link rel="icon" type="image/png" sizes="16x16" href="${require(`../../assets/images/icons/favicon-16x16.png`)}"> --}}
 
   <link rel="manifest" href="manifest.json">
   <meta name="theme-color" content="#ffffff">
   <meta name="msapplication-config" content="browserconfig.xml" />
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="../../assets/images/icons/ms-icon-144x144.png">
+  {{-- TODO <meta name="msapplication-TileImage" content="${require(`../../assets/images/icons/ms-icon-144x144.png`)}"> --}}
 
   {{-- <link rel="dns-prefetch" href="//fonts.googleapis.com"> --}}
   {{-- <link rel="dns-prefetch" href="//use.typekit.net"> --}}

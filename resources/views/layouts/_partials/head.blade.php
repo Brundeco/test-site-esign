ea9db174cc0201c4b534105b70297af503753cae
Bart Decorte <bart@esign.eu>
2020-11-03 11:41:37 +0100

    Add: print GTM/GA tags by variables

--- resources/views/layouts/_partials/head.blade.php
+++ resources/views/layouts/_partials/head.blade.php
@@ -65,16 +65,27 @@
   {{-- not needed for webpack setup: --}}
   {{-- <link rel="stylesheet" href="../assets/css/style.css"> --}}
 
+  {{-- Google Tag Manager --}}
+  @if ($googleTagManagerCode)
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','{{ $googleTagManagerCode }}');
+    </script>
+  @endif
 
-  {{-- google analytics --}}
-  {{-- <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  {{-- Google Analytics --}}
+  @if ($googleAnalyticsCode)
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-xxxxxxxx-x', 'auto');
-    ga('require', 'displayfeatures');
-    ga('send', 'pageview');
-  </script> --}}
+      ga('create', '{{ $googleAnalyticsCode }}', 'auto');
+      ga('require', 'displayfeatures');
+      ga('send', 'pageview');
+    </script>
+  @endif
 </head>

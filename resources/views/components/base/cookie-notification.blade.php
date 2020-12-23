0bbe7d2ddab1d3568c7d5442e915e0f93f9999d2
Brecht Waeghe <brecht@esign.eu>
2020-12-23 12:11:19 +0100

    Remove container from cookie notification

--- resources/views/components/base/cookie-notification.blade.php
+++ resources/views/components/base/cookie-notification.blade.php
@@ -1,59 +1,57 @@
-<div class="container">
-  <div class="cookie-notification hide | js-cookie-notification">
-    <div class="cookie-notification__content">
-      <div class="grid grid--gutter-half">
-        <div class="grid__item one-whole">
-          <span class="cookie-notification__title">Privacy instellingen</span>
-        </div>
-        <div class="grid__item one-whole">
-          <p>
-            Om u beter en persoonlijker te helpen, gebruiken wij cookies op onze website. Hiermee volgen wij en derde
-            partijen uw
-            internetgedrag op onze website en kunnen we uw ervaring verbeteren..
-          </p>
-        </div>
+<div class="cookie-notification hide | js-cookie-notification">
+  <div class="cookie-notification__content">
+    <div class="grid grid--gutter-half">
+      <div class="grid__item one-whole">
+        <span class="cookie-notification__title">Privacy instellingen</span>
+      </div>
+      <div class="grid__item one-whole">
+        <p>
+          Om u beter en persoonlijker te helpen, gebruiken wij cookies op onze website. Hiermee volgen wij en derde
+          partijen uw
+          internetgedrag op onze website en kunnen we uw ervaring verbeteren..
+        </p>
+      </div>
 
-        <div class="grid__item one-whole">
-          <div class="input-group">
-            <div class="checkbox">
-              <input type="checkbox" class="checkbox__toggle | js-cookie__marketing" name="cookie__marketing"
-                id="cookie__marketing" value="1" required />
-              <label for="cookie__marketing" tabindex="0">Marketing Cookies</label>
-              <span class="checkbox__description">Deze cookies gebruiken we voor marketingdoeleinden. Ze zorgen ervoor
-                dat we advertenties kunnen
-                optimaliseren op basis
-                van uw surfgedrag.</span>
-            </div>
+      <div class="grid__item one-whole">
+        <div class="input-group">
+          <div class="checkbox">
+            <input type="checkbox" class="checkbox__toggle | js-cookie__marketing" name="cookie__marketing"
+              id="cookie__marketing" value="1" required />
+            <label for="cookie__marketing" tabindex="0">Marketing Cookies</label>
+            <span class="checkbox__description">Deze cookies gebruiken we voor marketingdoeleinden. Ze zorgen ervoor
+              dat we advertenties kunnen
+              optimaliseren op basis
+              van uw surfgedrag.</span>
           </div>
         </div>
-        <div class="grid__item one-whole">
-          <div class="input-group">
-            <div class="checkbox">
-              <input type="checkbox" class="checkbox__toggle | js-cookie__socials" name="cookie__socials"
-                id="cookie__socials" value="1" required />
-              <label for="cookie__socials" tabindex="0">Socials Cookies</label>
-              <span class="checkbox__description">Deze cookies worden geleverd door derden om bepaalde social
-                media-functies mogelijk te maken, zoals een
-                Twitter
-                'Tweet'-knop, een LinkedIn' Share'-knop of een YouTube-video. Met deze cookies kunnen sociale
-                netwerkkanalen uw
-                surfgedrag volgen.</span>
-            </div>
+      </div>
+      <div class="grid__item one-whole">
+        <div class="input-group">
+          <div class="checkbox">
+            <input type="checkbox" class="checkbox__toggle | js-cookie__socials" name="cookie__socials"
+              id="cookie__socials" value="1" required />
+            <label for="cookie__socials" tabindex="0">Socials Cookies</label>
+            <span class="checkbox__description">Deze cookies worden geleverd door derden om bepaalde social
+              media-functies mogelijk te maken, zoals een
+              Twitter
+              'Tweet'-knop, een LinkedIn' Share'-knop of een YouTube-video. Met deze cookies kunnen sociale
+              netwerkkanalen uw
+              surfgedrag volgen.</span>
           </div>
         </div>
-        <div class="grid__item one-whole">
-          <div class="cookie-notification__actions">
-            <button type="button" class="button | js-cookie-all">
-              Accepteer alle cookies &nbsp; 🚀
-            </button>
-          </div>
+      </div>
+      <div class="grid__item one-whole">
+        <div class="cookie-notification__actions">
+          <button type="button" class="button | js-cookie-all">
+            Accepteer alle cookies &nbsp; 🚀
+          </button>
         </div>
-        <div class="grid__item one-whole">
-          <div class="cookie-notification__actions">
-            <button type="button" class="button button--secondary | js-cookie-custom">
-              Bewaar mijn keuze &nbsp; 🍪
-            </button>
-          </div>
+      </div>
+      <div class="grid__item one-whole">
+        <div class="cookie-notification__actions">
+          <button type="button" class="button button--secondary | js-cookie-custom">
+            Bewaar mijn keuze &nbsp; 🍪
+          </button>
         </div>
       </div>
     </div>

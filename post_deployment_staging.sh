3f088f8da0ebf99787ab440db461880447927b77
Bart Decorte <bart@esign.eu>
2021-01-08 17:27:22 +0100

    Update: npm install --no-optional flag in staging deploy script as well

--- post_deployment_staging.sh
+++ post_deployment_staging.sh
@@ -17,7 +17,7 @@ nvm install
 
 # Install npm dependencies
 echo "Installing npm dependencies..."
-npm install --production --silent
+npm install --no-optional --silent
 
 # Rebuild in case we switched npm versions
 echo "Rebuilding npm dependencies..."
